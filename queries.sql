c3dc6eea05189a0487434f43833abacef413661d
Owen Dougherty <76958825+Owen-Doc@users.noreply.github.com>
2021-07-09 18:58:18 -0400

    Queries to build preliminary table

---
+++ queries.sql
@@ -0,0 +1,11 @@
+create table athlete_data (
+	Type varchar(255),
+	Sex varchar(255),
+	FullName varchar(255),
+	UsedName varchar(255),
+	Nicknames varchar(255),
+	Born varchar(255),
+	Measurements varchar(255),
+	Affiliations varchar(255),
+	NOC varchar(255)
+);
