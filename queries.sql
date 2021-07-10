04d24f27be8e8dec06514a0cd1e2b2dcb3b32a77
Owen Dougherty <76958825+Owen-Doc@users.noreply.github.com>
2021-07-09 20:52:43 -0400

    Use this queries.sql to build the table in pgadmin

--- queries.sql
+++ queries.sql
@@ -1,14 +1,3 @@
-create table athlete_data (
-	Type varchar(255),
-	Sex varchar(255),
-	FullName varchar(255),
-	UsedName varchar(255),
-	Nicknames varchar(255),
-	Born varchar(255),
-	Measurements varchar(255),
-	Affiliations varchar(255),
-	NOC varchar(255)
-);
 
 create table athlete_data (
 	ID integer,
