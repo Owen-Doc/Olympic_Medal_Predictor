656dabe0ac38cec2594aaeaccbb04dcca6e4a602
Owen Dougherty <76958825+Owen-Doc@users.noreply.github.com>
2021-07-09 20:51:40 -0400

    csv can be loaded into postgres server, cleaning_athlete_data is loaded, cleaned, exported, df

--- queries.sql
+++ queries.sql
@@ -9,3 +9,23 @@ create table athlete_data (
 	Affiliations varchar(255),
 	NOC varchar(255)
 );
+
+create table athlete_data (
+	ID integer,
+	Name varchar(255),
+	Sex varchar(255),
+	Age varchar(255),
+	Height integer,
+	Weight integer,
+	Team varchar(255),
+	NOC varchar(255),
+	Games varchar(255),
+	Year integer,
+	Season varchar(255),
+	City varchar(255),
+	Sport varchar(255),
+	Event varchar(255),
+	Medal_Type varchar(255),
+	Medal varchar(255),
+	Row integer
+);
