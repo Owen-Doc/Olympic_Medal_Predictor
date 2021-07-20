8f8892e0dba6789e5dde15d9c48ec5b7f25f763e
Paul Do <9637489+paulviet@users.noreply.github.com>
2021-07-19 22:50:58 -0400

    adding chart and saving model

---
+++ queries.sql
@@ -0,0 +1,20 @@
+
+create table athlete_data (
+	ID integer,
+	Name varchar(255),
+	Sex varchar(255),
+	Age varchar(255),
+	Height integer,
+	Weight integer,
+	Team varchar(255),
+	NOC varchar(255),
+	Games varchar(255),
+	Year integer,
+	Season varchar(255),
+	City varchar(255),
+	Sport varchar(255),
+	Event varchar(255),
+	Medal_Type varchar(255),
+	Medal varchar(255),
+	Row integer
+);
